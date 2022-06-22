3b38ed72faee4d13fe63e71d50eff53e0b7ac2a5
Moaed-khola <101525274+Moaed-khola@users.noreply.github.com>
2022-06-23 01:58:18 +0300

    ds

--- db.php
+++ db.php
@@ -1,12 +1,12 @@
 <?php
 
-$servername = "192.168.43.44:17774";
+$servername = "192.168.43.44";
 $username = "root";
 $password = "2121";
 $db = "onlineshop";
 
 // Create connection
-$con = mysqli_connect($servername, $username, $password,$db);
+$con = mysqli_connect($servername, $username, $password,$db,15508);
 
 // Check connection
 if (!$con) {
