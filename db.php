bd4e840ab775e14f5ef1c0bbe04f549699eea8b3
Moaed-khola <101525274+Moaed-khola@users.noreply.github.com>
2022-06-27 10:03:11 +0300

    done

--- db.php
+++ db.php
@@ -6,7 +6,7 @@ $password = "2121";
 $db = "onlineshop";
 
 // Create connection
-$con = mysqli_connect($servername, $username, $password,$db,40419);
+$con = mysqli_connect($servername, $username, $password,$db,40412);
 
 // Check connection
 if (!$con) {
